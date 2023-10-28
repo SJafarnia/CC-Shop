eef4f9f9de6cdbfd0c673590b64dcaf12360ccfb
Sadeq Jafarnia <s.jafarnia95@gmail.com>
2023-10-28 12:11:54 +0330

    edit

--- src/app/(extras)/faqs/page.tsx
+++ src/app/(extras)/faqs/page.tsx
@@ -2,7 +2,7 @@
 import Breadcrumb from "@/components/layout/Breadcrumb"
 // import dynamic from "next/dynamic"
 import { Metadata } from "next/types"
-import FaqsTemplate from "../../../components/templates/extras/FaqsTemplate"
+import FaqsTemplate from "@/components/templates/FaqsTemplate"
 // const DynamicComponent = dynamic(() => import("../../../components/templates/extras/FaqsTemplate"), {
 //     ssr: false
 // })
