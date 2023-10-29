12f64267ebb840a74ba1bef1f70dcfc97fe11b6d
Sadeq Jafarnia <s.jafarnia95@gmail.com>
2023-10-30 00:57:43 +0330

    auth pages edit

--- src/components/modules/auth/login/LoginForm.tsx
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -131,7 +131,7 @@ function LoginForm() {
                                             <div className="flex items-center justify-between pb-6">
                                                 <p className="mb-0 mr-2">{"Don't have an account?"}</p>
                                                 <button
-                                                    onClick={() => router.push("/register#regform")}
+                                                    onClick={() => router.push("/register")}
                                                     type="button"
                                                     className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700"
                                                     data-te-ripple-init
@@ -140,23 +140,6 @@ function LoginForm() {
                                                 </button>
                                             </div>
                                         </form>
-                                    </div>
-                                </div>
-
-                                <div
-                                    className="flex items-center login-button rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
-                                    style={{ background: "background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)" }}>
-                                    <div className="px-4 py-6 text-white md:mx-6 md:p-12">
-                                        <h4 className="mb-6 text-xl font-semibold">
-                                            We are more than just a company
-                                        </h4>
-                                        <p className="text-sm">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit, sed do eiusmod tempor incididunt ut labore et
-                                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                                            nostrud exercitation ullamco laboris nisi ut aliquip ex
-                                            ea commodo consequat.
-                                        </p>
                                     </div>
                                 </div>
                             </div>
--- src/components/modules/auth/register/RegisterPage.tsx
+++ src/components/modules/auth/register/RegisterPage.tsx
@@ -194,7 +194,7 @@ function RegisterPage() {
                                                 <p className="mb-0 mr-2">Already got an account?</p>
 
                                                 <button
-                                                    onClick={() => router.push("/login#loginform")}
+                                                    onClick={() => router.push("/login")}
                                                     type="button"
                                                     className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700"
                                                     data-te-ripple-init
